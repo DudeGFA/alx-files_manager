a6092e801a81393680d1ce47c2ac5bab36d4b9ca
God'sfavour Idowu-Agida <favouridowuagida5@gmail.com>
2023-04-06 23:49:33 +0100

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -2,13 +2,13 @@ import { MongoClient } from 'mongodb';
 
 const dbHOST = process.env.DB_HOST || 'localhost';
 const dbPORT = process.env.DB_PORT || 27017;
-const db = process.env.DB_DATABASE || 'files_manager'
+const db = process.env.DB_DATABASE || 'files_manager';
 const url = `mongodb://${dbHOST}:${dbPORT}`;
 
 class DBClient {
     constructor() {
-        this.client = new MongoClient(url, {userUnifiedTopology: true, useNewUrlParser: true});
-        this.client.connect(),then(() => {
+        this.client = new MongoClient(url, { userUnifiedTopology: true, useNewUrlParser: true });
+        this.client.connect().then(() => {
             this.db = this.client.db(`${db}`);
         }).catch((err) => {
             console.log(err);
@@ -16,21 +16,21 @@ class DBClient {
     }
 
     isAlive() {
-        return this.client.isConnected()
+        return this.client.isConnected();
     }
 
     async nbUsers() {
         const usersCollection = this.db.collection('users');
         const docCount = await usersCollection.countDocuments();
-        return docCount
+        return docCount;
     }
 
     async nbFiles() {
-        const filesCollection = this.db.collection('files')
-        const docCount = await filesCollection.countDocuments()
-        return docCount
+        const filesCollection = this.db.collection('files');
+        const docCount = await filesCollection.countDocuments();
+        return docCount;
     }
 }
-const dbClient = new DBClient()
+const dbClient = new DBClient();
 
 module.exports = dbClient;
